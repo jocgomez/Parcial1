c2840b47865f820df4c14d2394bb01ad36d90f1d
jocgomez <jose_dav.gomez@uao.edu.co>
2020-09-08 15:51:13 -0500

    Pagina examen glicemia

--- app/src/main/java/com/example/parcial1/ExamenGlicemia.java
+++ app/src/main/java/com/example/parcial1/ExamenGlicemia.java
@@ -2,18 +2,13 @@ package com.example.parcial1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Punto1 extends Activity {
+public class ExamenGlicemia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_punto1);
-    }
-
-    public void validarNivel(View view) {
+        setContentView(R.layout.activity_examen_glicemia);
     }
 }
